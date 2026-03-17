eb0186791ce73d240db332724c201f6a3a0aaa02
Sushilkumar <sushilkumartondikatti02@gmail.com>
2026-03-17 12:36:15 +0530

    Update LoginScreen.dart

--- LoginScreen.dart
+++ LoginScreen.dart
@@ -7,27 +7,40 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("vijaybadiger"),
+        title: Text("Kumar"),
         backgroundColor: Colors.green,
       ),
 
-      body: Column(
-        children: [
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
 
-          Image.network(
-            "https://upload.wikimedia.org/wikipedia/commons/7/7e/Virat_Kohli_ODI.jpg",
-            height: 200,
-          ),
-
-          Text(
-            "hii",
-            style: TextStyle(
-              color: Colors.red,
-              fontSize: 24,
+            Image.network(
+              "https://upload.wikimedia.org/wikipedia/commons/7/7e/Virat_Kohli_ODI.jpg",
+              height: 200,
             ),
-          ),
 
-        ],
+            SizedBox(height: 20),
+
+            Image.network(
+              "https://imgeng.jagran.com/images/2023/nov/kl-profile1700138082033.jpg",
+              height: 200,
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "hii",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 24,
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
