6eba273deaf231f5c4b72160892eb5569e6934b7
Sushilkumar <sushilkumartondikatti02@gmail.com>
2026-03-17 12:48:01 +0530

    Refactor LoginScreen layout with scrollable view

--- LoginScreen.dart
+++ LoginScreen.dart
@@ -11,35 +11,55 @@ class LoginScreen extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
 
-            Image.network(
-              "https://upload.wikimedia.org/wikipedia/commons/1/15/Virat_Kohli_portrait.jpg",
-              height: 200,
-            ),
+              SizedBox(height: 20),
 
-            SizedBox(height: 20),
-
-            Image.network(
-              "https://upload.wikimedia.org/wikipedia/commons/1/15/Virat_Kohli_portrait.jpg",
-              height: 200,
-            ),
-
-            SizedBox(height: 20),
-
-            Text(
-              "hii",
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 24,
+              Image.network(
+                "https://upload.wikimedia.org/wikipedia/commons/1/15/Virat_Kohli_portrait.jpg",
+                height: 200,
               ),
-            ),
 
-          ],
+              SizedBox(height: 20),
+
+              Image.network(
+                "https://upload.wikimedia.org/wikipedia/commons/1/15/Virat_Kohli_portrait.jpg",
+                height: 200,
+              ),
+
+              SizedBox(height: 20),
+
+              Text(
+                "hii",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 24,
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              // ✅ TextBox
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Enter your name",
+                    hintText: "Type here...",
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 40),
+
+            ],
+          ),
         ),
       ),
     );
