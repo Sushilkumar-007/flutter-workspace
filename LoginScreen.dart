2ce968fa938e15b862ae996221f9f12d48e9a787
Sushilkumar <sushilkumartondikatti02@gmail.com>
2026-03-17 12:10:14 +0530

    Update LoginScreen.dart

--- LoginScreen.dart
+++ LoginScreen.dart
@@ -1,15 +1,34 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
-@override
-  Widget build(buildContext context){
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:colors.red,
-      appBar: AppBar;(
-        title:text("yourname"),
-        backgroundColor: colors.green,
-        ) AppBar
-      }; Scaffold
-      }
-      }
+      appBar: AppBar(
+        title: Text("vijaybadiger"),
+        backgroundColor: Colors.green,
+      ),
+
+      body: Column(
+        children: [
+
+          Image.network(
+            "https://upload.wikimedia.org/wikipedia/commons/7/7e/Virat_Kohli_ODI.jpg",
+            height: 200,
+          ),
+
+          Text(
+            "hii",
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 24,
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
