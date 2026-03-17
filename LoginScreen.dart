e25414944b63020062aad14346fd41f4c2df710a
Sushilkumar <sushilkumartondikatti02@gmail.com>
2026-03-17 10:45:50 +0530

    Create LoginScreen widget with basic UI elements
    Initial implementation of the LoginScreen widget with a basic layout.

---
+++ LoginScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+@override
+  Widget build(buildContext context){
+    return Scaffold(
+      backgroundColor:colors.red,
+      appBar: AppBar;(
+        title:text("yourname"),
+        backgroundColor: colors.green,
+        ) AppBar
+      }; Scaffold
+      }
+      }
