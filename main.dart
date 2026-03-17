c67f18b39ce71d834c23ed0a167468cb10d776f2
Sushilkumar <sushilkumartondikatti02@gmail.com>
2026-03-17 10:37:22 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart':
+main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(buildContext){
+    return MaterialApp(
+      home:LoginScreen();
+      ); MaterialApp
+    }
+}
